4eb1d99d1603c88ddbbc2c65bbd52e441dd9228a
jy lim <imjee02@gmail.com>
2023-11-28 12:41:36 +0900

    refactor: Audience 캡슐화

--- src/main/java/com/jy/object/ticket/TicketSeller.java
+++ src/main/java/com/jy/object/ticket/TicketSeller.java
@@ -12,17 +12,9 @@ public class TicketSeller {
 
 
     public void sellTo(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().setTicket(ticket);
-
-        } else {
-            Ticket ticket = ticketOffice.getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketOffice.plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
-
+        Ticket ticket = ticketOffice.getTicket();
+        Long amount = audience.buy(ticket);
+        ticketOffice.minusAmount(amount);
     }
 
 }
