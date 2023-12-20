bc499e73d7e5c2904235081d122037157fae1509
jiyeong-dt <imjee02@gmail.com>
2023-12-20 19:48:56 +0900

    feat: 평균과 pass,fail count 구하는 클래스 추가

---
+++ src/test/java/com/jy/object/chapter12/polymorphism/LectureTest.java
@@ -0,0 +1,10 @@
+package com.jy.object.chapter12.polymorphism;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LectureTest {
+
+}
