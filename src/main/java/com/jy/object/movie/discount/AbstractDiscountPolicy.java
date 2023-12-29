f3105f03cc0da43b9977277e0833b84b1c2dcf38
jiyeong-dt <imjee02@gmail.com>
2023-12-29 22:59:52 +0900

    feat: 사전 조건과 사후 조건 추가

--- src/main/java/com/jy/object/movie/discount/AbstractDiscountPolicy.java
+++ src/main/java/com/jy/object/movie/discount/AbstractDiscountPolicy.java
@@ -16,14 +16,29 @@ public abstract class AbstractDiscountPolicy implements DiscountPolicy {
     }
 
     public Money calculateDiscountAmount(final Screening screening) {
+
+        checkPrecondition(screening);
+
         for (DiscountCondition discountCondition : discountConditions) {
-            if(discountCondition.isSatisfiedBy(screening)) {
-                return this.getDiscountAmount(screening);
+            if (discountCondition.isSatisfiedBy(screening)) {
+                Money discountAmount = this.getDiscountAmount(screening);
+                checkPostcondition(discountAmount);
+                return discountAmount;
             }
         }
-
         return Money.ZERO;
     }
+
+    //사후 조건
+    private void checkPostcondition(final Money discountAmount) {
+        assert discountAmount != null && discountAmount.isGreaterThanOrEqual(Money.ZERO);
+    }
+
+    //사전 조건
+    private void checkPrecondition(final Screening screening) {
+        assert screening != null;
+    }
+
 
     abstract protected Money getDiscountAmount(final Screening screening);
 }
